5a88b5b342f775dd2a9d75bee5beadcc71c684b4
guillaume <guillaumesamtmann24@gmail.com>
2025-05-15 09:57:44 +0200

    final projet

--- Assets/script/BtnAnim.cs
+++ Assets/script/BtnAnim.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 public class BtnAnim : MonoBehaviour
 {
     public Animator Anim;
+    public bool isWalk = false;
+    public bool isJump = false;
 
     public void Walk()
     {
-        Anim.SetTrigger("walkTriggeur");
+        if (!isWalk || isJump)
+        {
+            Anim.SetBool("isWalk", true);
+            isWalk = true;
+            Anim.SetBool("isJump", false);
+            isJump = false;
+        }
+    }
+
+    public void Stop()
+    {
+        if (isWalk)
+        {
+            Anim.SetBool("isWalk", false);
+            isWalk = false;
+        }
+        else if (isJump)
+        {
+            Anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 
     public void jump()
     {
-        Anim.SetTrigger("jumpTrigeur");
+        if (!isJump || isWalk)
+        {
+            Anim.SetBool("isWalk", false);
+            Anim.SetBool("isJump", true);
+            isJump = true;
+            isWalk = false;
+        }
     }
 
 }
